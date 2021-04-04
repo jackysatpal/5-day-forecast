e3415d6c84ee62613b0e0b433da8b82de9391571
jackysatpal <jackysatpal.212@gmail.com>
2021-04-04 11:13:38 -0500

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '859693c2f1bc4a3ad8d7da759675651a';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
